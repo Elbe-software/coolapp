fb1d13cf6bf16c0fdec76b2b273ee68f4b757777
Elbe-software <elias.lechevallier@mail.mcgill.ca>
2025-01-25 13:42:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar />
+      
+
+      <h1>
+        Intro to React ft. Ian and Jake
+      </h1>
+      <h2>
+        God, what a good looking website
+      </h2>
+
+
+    </div>
+  );
+}
+
+export default App;
